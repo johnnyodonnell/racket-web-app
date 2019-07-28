b53bd88b4d6e8e9b0ca9f5fc960a2deb1e0b6d9a
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-07-28 10:28:14 -0700

    Abstract runtime into a layer and depend on that layer

--- upload.sh
+++ upload.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-docker rm runtime-container
-rm -f bootstrap
-
-docker build . --tag runtime-image
-docker create -it --name runtime-container runtime-image
-docker cp runtime-container:/runtime bootstrap
-
-zip -r lambda.zip bootstrap index.rkt
-aws lambda update-function-code --function-name racket-web-app --zip-file fileb://lambda.zip --region us-west-2
+zip -r lambda.zip index.rkt
+aws lambda update-function-code --function-name racket-web-app \
+    --zip-file fileb://lambda.zip --region us-west-2
 
